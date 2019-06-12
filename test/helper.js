fd820fd3ba16ca782770dc50bfda7ed94a5cc059
Martin Jonsson <martin.jonsson@gmail.com>
2019-06-12 15:13:36 +0200

    Add support for specifying output format

--- test/helper.js
+++ test/helper.js
@@ -2,8 +2,15 @@
 const readChunk = require('read-chunk');
 const fileType = require('file-type');
 
-module.exports.isJPG = function (file) {
-	const buffer = readChunk.sync(file, 0, 4100);
+function is(file, extension) {
+	const buffer = readChunk.sync(file, 0, fileType.minimumBytes);
+	return fileType(buffer).ext === extension;
+}
 
-	return fileType(buffer).ext === 'jpg';
+exports.isJPG = function (file) {
+	return is(file, 'jpg');
+};
+
+exports.isPNG = function (file) {
+	return is(file, 'png');
 };
